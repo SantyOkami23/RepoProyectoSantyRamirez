79164949e4d2a59fc3475ac15a1212872c863aee
SantyOkami23 <santiago.ramirez5@utp.edu.co>
2025-03-26 19:00:49 -0500

    Create se agregan excepciones

---
+++ src/main/java/co/pruebatecnica/exception/DuplicateTaskException.java
@@ -0,0 +1,7 @@
+package co.pruebatecnica.exception;
+
+public class DuplicateTaskException extends Exception {
+    public DuplicateTaskException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/co/pruebatecnica/exception/TaskNotFoundException.java
@@ -0,0 +1,7 @@
+package co.pruebatecnica.exception;
+
+public class TaskNotFoundException extends Exception {
+    public TaskNotFoundException(String message) {
+        super(message);
+    }
+}
